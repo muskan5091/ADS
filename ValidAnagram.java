9a3d1d0e3f5c4e24f6f8a9a398b4de45fa6ffef6
muskan5091 <156787198+muskan5091@users.noreply.github.com>
2026-02-11 21:43:12 +0530

    Implement isAnagram method for anagram checking

---
+++ ValidAnagram.java
@@ -0,0 +1,28 @@
+class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+    //   if(s.length()!=t.length())
+    //   return false;
+    //   char sarr[] =s.toCharArray();
+    //    char tarr[] =t.toCharArray();
+    //    Arrays.sort(sarr);
+    //    Arrays.sort(tarr);          time complexity - zyda hai 
+    //    return Arrays.equals(sarr, tarr);
+
+       if(s.length()!=t.length())
+       return false;
+
+       int[] freq=new int[26]; //ch 26 hote hai
+       for(char c: s.toCharArray()){
+        freq[c-'a']++;
+       }
+       for(char c: t.toCharArray()){
+        freq[c-'a']--;
+       }
+       for(int x: freq)
+       if(x!=0)
+       return false;
+       return true;
+
+
+    }
+}
