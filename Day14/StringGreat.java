cc0eb0e73fb6f28392047bda01d28af82adc364f
muskan5091 <156787198+muskan5091@users.noreply.github.com>
2026-03-13 15:35:35 +0530

    LeetCode- 1544. Make The String Great

---
+++ Day14/StringGreat.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String makeGood(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+            
+            if(sb.length() > 0 && 
+               Math.abs(sb.charAt(sb.length()-1) - c) == 32){
+                
+                sb.deleteCharAt(sb.length()-1); // remove bad pair
+            }
+            else{
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
